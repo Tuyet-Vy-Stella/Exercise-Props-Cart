3d690772236ceb96bc65dee70403bf568580fe3b
Tuyet-Vy <nhuochanvu@gmail.com>
2022-08-11 03:46:01 +0700

    complete layout

---
+++ src/ExerciseProductCart/CartModal.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+
+export default class CartModal extends Component {
+  render() {
+    return (
+      <div>
+        <div
+          className="modal fade"
+          id="modelId"
+          tabIndex={-1}
+          role="dialog"
+          aria-labelledby="modelTitleId"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog" role="document">
+            <div
+              className="modal-content"
+              style={{ width: "800px", maxWidth: "800px" }}
+            >
+              <div className="modal-header">
+                <h5 className="modal-title">Giỏ hàng</h5>
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div className="modal-body">
+                <table className="table">
+                    <tr>
+                        <td>Mã sản phẩm</td>
+                        <td>Hình ảnh</td>
+                        <td>Tên sản phẩm</td>
+                        <td>Số lượng</td>
+                        <td>Đơn giá</td>
+                        <td>Thành tiền</td>
+                    </tr>
+                </table>
+              </div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  data-bs-dismiss="modal"
+                >
+                  Close
+                </button>
+                <button type="button" className="btn btn-primary">
+                  Save
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/ExerciseProductCart/ExerciseProductCart.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import CartModal from './CartModal'
+import ProductList from './ProductList'
+
+const data = [
+    { "maSP": 1, "tenSP": "VinSmart Live", "manHinh": "AMOLED, 6.2, Full HD+", "heDieuHanh": "Android 9.0 (Pie)", "cameraTruoc": "20 MP", "cameraSau": "Chính 48 MP & Phụ 8 MP, 5 MP", "ram": "4 GB", "rom": "64 GB", "giaBan": 5700000, "hinhAnh": "./img/vsphone.jpg" },
+    { "maSP": 2, "tenSP": "Meizu 16Xs", "manHinh": "AMOLED, FHD+ 2232 x 1080 pixels", "heDieuHanh": "Android 9.0 (Pie); Flyme", "cameraTruoc": "20 MP", "cameraSau": "Chính 48 MP & Phụ 8 MP, 5 MP", "ram": "4 GB", "rom": "64 GB", "giaBan": 7600000, "hinhAnh": "./img/meizuphone.jpg" },
+    { "maSP": 3, "tenSP": "Iphone XS Max", "manHinh": "OLED, 6.5, 1242 x 2688 Pixels", "heDieuHanh": "iOS 12", "cameraSau": "Chính 12 MP & Phụ 12 MP", "cameraTruoc": "7 MP", "ram": "4 GB", "rom": "64 GB", "giaBan": 27000000, "hinhAnh": "./img/applephone.jpg" }
+]
+
+export default class ExerciseProductCart extends Component {
+  render() {
+    return (
+      <div className='container'>
+        <h1>Exercise Products Cart</h1>
+        <CartModal/>
+        <div className='text-end'>
+            <span className='text-danger' style={{fontSize:'20px', fontWeight:'bold', cursor:'pointer'}} data-bs-toggle="modal" data-bs-target="#modelId">Giỏ Hàng (0)</span>
+        </div>
+        <ProductList arrProduct={data}/>
+      </div>
+    )
+  }
+}
